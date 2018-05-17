dfa90a58827ed45df914d258058e04a94951d141
Trisnayp <trisnayulianti504@gmail.com>
2018-05-17 10:57:26 +0700

    distroit

---
+++ customer/footer.php
@@ -0,0 +1,141 @@
+<!-- start: Footer -->
+	<div id="footer">
+		
+		<!-- start: Container -->
+		<div class="container">
+			
+			<!-- start: Row -->
+			<div class="row">
+
+				<!-- start: About -->
+				<div class="span3">
+					
+					<h3>Toko Online DistroIT</h3>
+					<p>
+					Kami merupakan toko online dan distributor outlet, yang memproduksi dan memasarkan sendiri produk kami dengan menggunakan Teknik Sablon DTG yang meliputi Kaos, Kemeja, Kaos Kerah, Sweater, dll.	
+                    </p>
+						
+				</div>
+				<!-- end: About -->
+
+				<!-- start: Photo Stream -->
+				<div class="span3">
+					
+					<h3>Alamat Kami</h3>
+                    Perum BKI Desa Sukamanah Kec. Sukatani Kabupaten Bekasi 17530<br />
+                    Telp/SMS/WA/TG : 0857 949 848 03<br />
+                    Website : <a href="http://www.hakkoblogs.com">www.hakkoblogs.com</a>
+                    </div>
+				<!-- end: Photo Stream -->
+
+				<div class="span6">
+				
+					<!-- start: Follow Us -->
+					<h3>Follow Us!</h3>
+					<ul class="social-grid">
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-twitter">
+											<a href="http://twitter.com"></a>
+										</div>
+										<div class="social-info-back social-twitter-hover">
+											<a href="http://twitter.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-linkedin">
+											<a href="http://facebook.com"></a>
+										</div>
+										<div class="social-info-back social-linkedin-hover">
+											<a href="http://facebook.com"></a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-dribbble">
+											<a href="http://dribble.com"></a>
+										</div>
+										<div class="social-info-back social-dribble-hover">
+											<a href="http://dribble.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+						<li>
+							<div class="social-item">				
+								<div class="social-info-wrap">
+									<div class="social-info">
+										<div class="social-info-front social-flickr">
+											<a href="http://flickr.com"></a>
+										</div>
+										<div class="social-info-back social-flickr-hover">
+											<a href="http://flickr.com"></a>
+										</div>	
+									</div>
+								</div>
+							</div>
+						</li>
+					</ul>
+					<!-- end: Follow Us -->
+				
+					<!-- start: Newsletter -->
+				<!--	<form id="newsletter">
+						<h3>Newsletter</h3>
+						<p>Please leave us your email</p>
+						<label for="newsletter_input">@:</label>
+						<input type="text" id="newsletter_input"/>
+						<input type="submit" id="newsletter_submit" value="submit">
+					</form> -->
+					<!-- end: Newsletter -->
+				
+				</div>
+				
+			</div>
+			<!-- end: Row -->	
+			
+		</div>
+		<!-- end: Container  -->
+
+	</div>
+	<!-- end: Footer -->
+
+	<!-- start: Copyright -->
+	<div id="copyright">
+	
+		<!-- start: Container -->
+		<div class="container">
+		
+			<p>
+				Copyright &copy; <a href="http://bootstrapmaster.com" alt="Bootstrap Themes">Toko Online DistroIT &copy; 2017 </a> Official Website
+			</p>
+	
+		</div>
+		<!-- end: Container  -->
+		
+	</div>	
+	<!-- end: Copyright -->
+    
+    <!-- start: Java Script -->
+<!-- Placed at the end of the document so the pages load faster -->
+<script src="../js/jquery-1.8.2.js"></script>
+<script src="../js/bootstrap.js"></script>
+<script src="../js/flexslider.js"></script>
+<script src="../js/carousel.js"></script>
+<script src="../js/jquery.cslider.js"></script>
+<script src="../js/slider.js"></script>
+<script defer="defer" src="../js/custom.js"></script>
+<!-- end: Java Script -->
